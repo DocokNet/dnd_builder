406aa2a646b6989bbb297f0c96ec71c19cecea21
DocokNet <misha.romanchuk.2015@mail.ru>
2024-12-19 19:48:53 +0300

    added serializers

--- WebDnd/dndpage/models.py
+++ WebDnd/dndpage/models.py
@@ -8,6 +8,9 @@ class Races(models.Model):
     cons = models.JSONField(default=dict())
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Background(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
@@ -16,29 +19,45 @@ class Background(models.Model):
     instruments_prof = models.JSONField(default=list())
     background_gear = models.JSONField(default=list())
 
+    def __str__(self):
+        return self.name
+
 class Archetypes(models.Model):
     id = models.AutoField(primary_key=True)
     archetype_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.archetype_name
 
 class Classes(models.Model):
     id = models.AutoField(primary_key=True)
     class_name = models.CharField(max_length=20)
     hp_dice = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.class_name
+
 class Class_skills(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Archetype_skills(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     archetype_id = models.ForeignKey(Archetypes, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Current_class_info(models.Model):
     id = models.AutoField(primary_key=True)
     class_id = models.ForeignKey(Class_skills, on_delete=models.CASCADE)
     archetype_id = models.ForeignKey(Archetype_skills, on_delete=models.CASCADE)
+
 
 class Player(models.Model):
     list_id = models.AutoField(primary_key=True)
@@ -52,3 +71,6 @@ class Player(models.Model):
     stats = models.JSONField(default=dict())
     skills = models.JSONField(default=dict())
     gear = models.JSONField(default=dict())
+
+    def __str__(self):
+        return self.character_name
---
+++ WebDnd/dndpage/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Races, Background, Archetypes, Classes, Class_skills, Archetype_skills, Current_class_info, Player
+
+class RacesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Races
+        fields = ['id', 'name', 'pros', 'cons', 'description']
+
+class BackgroundSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Background
+        fields = ['id', 'name', 'background_skills', 'languages', 'instruments_prof', 'background_gear']
+
+class ArchetypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Archetypes
+        fields = ['id', 'archetype_name']
+
+class ClassesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classes
+        fields = ['id', 'class_name', 'hp_dice']
+
+class ClassSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class_skills
+        fields = ['id', 'name', 'class_id']
+
+class ArchetypeSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Archetype_skills
+        fields = ['id', 'name', 'archetype_id']
+
+class CurrentClassInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Current_class_info
+        fields = ['id', 'class_id', 'archetype_id']
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['list_id', 'character_name', 'level', 'class_table', 'race_id', 'background_id', 'alignment', 'experience', 'stats', 'skills', 'gear']
